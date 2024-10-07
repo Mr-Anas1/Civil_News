1abdc73eeb663b19718ef8df92d0cdb009e51b17
Mohamed Anas <a3689211@gmail.com>
2024-10-07 17:56:13 +0530

    some changes made

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './Contact.css'
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <div className="contact-description">
+        <h1>Contact us</h1>
+        <p>Fill out the form to contact us</p>
+      </div>
+
+      <div className="contact-main">
+
+        <div className="contact-info">
+          <h1> Info</h1>
+          <div><FaPhoneAlt /><p>01234567890</p></div>
+          <div><IoIosMail /><p>hello@gmail.com</p></div>
+          <div><FaLocationDot /><p>Massachusetts Ave NW, Washington, DC 20036</p></div>
+        </div>
+
+        <div className="contact-form">
+          <div className="input-name">
+            <input type="text" className='input-firstName' placeholder='First name' />
+            <input type="text" className='input-lastName' placeholder='Second name'/>
+          </div>
+          <input type="text" className="input-subject" placeholder='Subject'/>
+          <textarea name="description" className="input-description" placeholder='Description'></textarea>
+          <button className="input-btn">Send Message</button>
+        </div>
+      </div>
+    </div>
   )
 }
 

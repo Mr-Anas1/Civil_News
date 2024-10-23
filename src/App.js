a2d4637bc2d62c7e05ab6b3600cdca00c5970a99
Mohamed Anas <a3689211@gmail.com>
2024-10-23 14:33:47 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       
         <Navbar />
         <Routes>
-          <Route path={"/home"} element={<Home />} />
+          <Route path={"/listen"} element={<Home />} />
           <Route path={"/podcast"} element={<Podcast />}>
             <Route path={":podcastId"} element={<Podcast />}/>
           </Route>
